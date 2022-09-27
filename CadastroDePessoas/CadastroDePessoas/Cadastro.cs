af7352a00a78746c15254f7a31fc88a1991edde9
Amanda Luiza <77518236+AmandaLuiza@users.noreply.github.com>
2022-09-27 15:27:33 -0300

    Add files via upload

---
+++ CadastroDePessoas/CadastroDePessoas/Cadastro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDePessoas
+{
+    
+    public class Cadastro
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand cmd = new SqlCommand();
+        public String mensagem;
+        public Cadastro(String nome, String telefone)
+        {
+            // TODO: Criar um crud/comandos ---SqlCommand 
+            cmd.CommandText = "";
+            // TODO: inserir parametros
+            // TODO: conectar com o banco de dados -- Conexao
+            // TODO: executar o comando escrito 
+            // TODO: desconectar
+            // TODO: mostrar mensagem de erro ou sucesso -- variavel
+        }
+    }
+}
---
+++ CadastroDePessoas/CadastroDePessoas/Conexao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDePessoas
+
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+        public Conexao()
+        {
+            con.ConnectionString = @"Data Source=LAPTOP-5BR8SEUU\\SQLEXPRESS;Initial Catalog=Cadastro;Integrated Security=True";
+        }
+        public SqlConnection conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+
+        }
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
