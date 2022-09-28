d1b9d014310b47c33bce3fd7c22ee485cf47252f
Amanda Luiza <77518236+AmandaLuiza@users.noreply.github.com>
2022-09-28 11:40:52 -0300

    Add files via upload

---
+++ CadastroDePessoas-main/CadastroDePessoas-main/CadastroDePessoas/CadastroDePessoas/Cadastro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDePessoas
+{
+    
+    public class Cadastro
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand cmd = new SqlCommand();
+        public String mensagem = "";
+        public Cadastro(String nome, String telefone)
+        {
+            // TODO: Criar um crud/comandos ---SqlCommand 
+            cmd.CommandText = "insert into ex (nome, telefone) values(@nome, @telefone) ";
+            // TODO: inserir parametros
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@telefone", telefone);
+
+            // TODO: conectar com o banco de dados -- Conexao
+            try
+            {
+                cmd.Connection = conexao.conectar();
+                // TODO: executar o comando escrito 
+                cmd.ExecuteNonQuery();
+                // TODO: desconectar
+                conexao.desconectar();
+                // TODO: mostrar mensagem de erro ou sucesso -- variavel
+                this.mensagem = "Cadastrado com sucesso";
+            }
+            catch (SqlException e)
+            {
+                this.mensagem = "Houve um problema, não foi cadastrado";
+            }
+
+
+
+        }
+    }
+}
---
+++ CadastroDePessoas-main/CadastroDePessoas-main/CadastroDePessoas/CadastroDePessoas/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroDePessoas
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Cadastro cad = new Cadastro(txbNome.Text, txbTelefone.Text);
+            MessageBox.Show(cad.mensagem);
+        }
+    }
+}
